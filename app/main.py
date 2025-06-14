63045f9a5319b38141966a6202680b1602793149
mkcomru <grebenshchikov.my@students.dvfu.ru>
2025-06-15 01:31:00 +1000

    add pydantic schemas for authors and books

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
-# from fastapi import FastAPI, Request
-# # taskkill /IM python.exe /F
+from fastapi import FastAPI, Request
+# taskkill /IM python.exe /F
 
-# app = FastAPI()
+app = FastAPI()
 
 
-# @app.post("/register")
-# async def register(request: Request):
-#     data = await request.json()
-#     print(data)
-#     return {"message": "User registered successfully"}
+
+
+
 
 
 
